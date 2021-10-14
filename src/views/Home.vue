dd19a7ee21b27ca0bc445d7b9ad6e8b6bfcf94f9
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-14 22:16:51 +0100

    Use Expenses component in Home view. Pass in expenses prop into component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
     <ExpenseForm msg="Welcome to Your Vue.js App" />
+    <Expenses :expenses="expenses"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ExpenseForm from "@/components/ExpenseForm.vue";
+import Expenses from "@/components/Expenses.vue";
 
 export default {
   name: "Home",
   components: {
     ExpenseForm,
+    Expenses
   },
+  data() {
+    return {
+      expenses: []
+    }
+  }
 };
 </script>
