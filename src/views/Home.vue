29cf698a4ec6dbfc0a3e8d9f78e9f64303215b04
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-14 22:17:39 +0100

    Use created lifecycle method to set initial value for expenses state (temporarily)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,6 +20,31 @@ export default {
     return {
       expenses: []
     }
+  },
+  created() {
+    this.expenses = [
+      {
+        id: 1,
+        name: 'Arup September pay',
+        date: '2021-09-15',
+        cost: 2000,
+        type: 'income',
+      },
+      {
+        id: 2,
+        name: 'Tesco food shopping',
+        date: '2021-10-01',
+        cost: 50.99,
+        type: 'expenditure',
+      },
+      {
+        id: 3,
+        name: 'Red Hot Chili Peppers tickets',
+        date: '2021-10-13',
+        cost: 190,
+        type: 'expenditure',
+      },
+    ]
   }
 };
 </script>
