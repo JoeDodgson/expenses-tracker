da6641d4291c80abf0ed6d7aec89c5b4f0b8e8f2
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-14 21:34:52 +0100

    Create Button component with props of text, color, type

---
+++ src/components/Button.vue
@@ -0,0 +1,17 @@
+<template>
+    <button :style="{ background: color }" :type="type">
+        {{ text }}
+    </button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String,
+        color: String,
+        type: String
+    }
+}
+</script>
+
