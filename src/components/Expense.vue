ef336727eb227ecfd51d0b2d6931268db058411d
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-17 22:18:40 +0100

    Create separate component for individual expense

---
+++ src/components/Expense.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>{{ name }} - £{{ cost }} - {{ date }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Expense",
+  props: ["name", "title", "date"],
+};
+</script>
