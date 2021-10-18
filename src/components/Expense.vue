196a21104ff159da82766d472d0bfad3d53d8f77
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-18 10:01:12 +0100

    Fix cost prop in expense container and add basic styling

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="expense-card">
     <h3>{{ name }} - £{{ cost }} - {{ date }}</h3>
   </div>
 </template>
@@ -7,6 +7,17 @@
 <script>
 export default {
   name: "Expense",
-  props: ["name", "title", "date"],
+  props: ["name", "cost", "date"],
 };
 </script>
+
+<style>
+  .expense-card {
+    background: #f4f4f4;
+    margin: 5px 10px;
+    padding: 10px 20px;
+    cursor: pointer;
+    border: 2px solid #000;
+    border-radius: 2.5px;
+  }
+</style>
