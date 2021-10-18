dc72bdb1e29c375c1fbfefb384a7b52ac9e36888
Joe Dodgson <j.dodgson@live.co.uk>
2021-10-18 11:52:27 +0100

    Add title to expenses container component. Display expenses as green for income, red for expenses. Update styling

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="expense-card">
-    <h3>{{ name }} - {{ cost }} - {{ date }}</h3>
+  <div class="expense-card" :style="{ background: type === 'income' ? 'rgb(0, 128, 0, 0.2)' : 'rgba(255, 0, 0, 0.2)' }">
+    <div class="expense-details">
+      <h3>{{ type === 'income' ? '+' : '-'}} {{ cost }}</h3>
+      <p>{{ name }}</p>
+      <p>{{ date }}</p>
+    </div>
     <div>
-      <Button text="Modify" color="grey"/>
-      <Button text="Delete" color="red"/>
+      <Button text="Modify" color="rgb(200 200 200)"/>
+      <Button text="Delete" color="rgb(200 200 200)"/>
     </div>
   </div>
 </template>
@@ -15,19 +19,22 @@ export default {
   components: {
     Button
   },
-  props: ["name", "cost", "date"],
+  props: ["name", "cost", "date", "type"],
 };
 </script>
 
 <style>
   .expense-card {
-    background: #f4f4f4;
-    margin: 5px 10px;
+    margin: 5px 0;
     padding: 10px 20px;
     cursor: pointer;
     border: 2px solid #000;
     border-radius: 2.5px;
     display: flex;
     justify-content: space-between;
+  }
+  .expense-details h3, p {
+    margin: 5px 0;
+    text-align: left;
   }
 </style>
--- src/components/ExpensesContainer.vue
+++ src/components/ExpensesContainer.vue
@@ -1,6 +1,7 @@
 <template>
-  <div :key="expense.id" v-for="expense in expenses">
-    <Expense :name="expense.name" :cost="expense.costFormatted" :date="expense.date"/>
+  <div class="expenses-container">
+    <h3 class="left-title">Saved Expenses</h3>
+    <Expense :key="expense.id" v-for="expense in expenses" :name="expense.name" :cost="expense.costFormatted" :date="expense.date" :type="expense.type"/>
   </div>
 </template>
 
@@ -16,3 +17,12 @@ export default {
   },
 };
 </script>
+
+<style>
+.left-title {
+  text-align: left;
+}
+.expenses-container {
+  padding: 15px;
+}
+</style>
