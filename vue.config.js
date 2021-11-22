8dabae9a1742e51eb7ececac746ccc35779d87a0
Joe Dodgson <j.dodgson@live.co.uk>
2021-11-22 22:20:33 +0000

    Remove SASS file imports from vue.config.js because they are already imported in src/quasar-user-options.js

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,9 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `
-            @import "@/sass/quasar.variables.sass"
-            @import "@/sass/quasar.sass"
-        `,
-      },
-    },
-  },
-
   pluginOptions: {
     quasar: {
       importStrategy: "kebab",
       rtlSupport: false,
     },
   },
-
   transpileDependencies: ["quasar"],
 };
