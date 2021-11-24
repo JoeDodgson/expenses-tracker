ee757112c77fbd2ecaae7ac4c482e785aa4c398a
Joe Dodgson <j.dodgson@live.co.uk>
2021-11-24 11:56:42 +0000

    Change the style of expense cards to be more professional

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -2,21 +2,30 @@
   <div class="row">
     <div class="col-grow">
       <q-card class="my-card">
-        <q-card-section
-          :class="[expense.type === 'income' ? 'bg-green' : 'bg-red', 'text-white']"
-        >
-          <div class="text-h6">
-            {{ expense.type === "income" ? "+" : "-" }} {{ expense.formattedCost }}
+        <q-card-section class="justify-between">
+          <div class="row">
+            <div class="col-1 text-left">
+              <q-btn flat @click="$emit('delete-expense', expense.id)">
+                <i class="fa fa-trash" aria-hidden="true"></i>
+              </q-btn>
+            </div>
+            <div class="col-8 text-left">
+              <div class="text-h5">{{ expense.name }}</div>
+              <div class="text-subtitle2">{{ expense.date }}</div>
+            </div>
+            <div
+              :class="[
+                expense.type === 'income' ? 'text-positive' : 'text-negative',
+                'text-h6',
+                'col-3',
+                'text-right',
+              ]"
+            >
+              {{ expense.type === "income" ? "+" : "-" }}
+              {{ expense.formattedCost }}
+            </div>
           </div>
-          <div class="text-subtitle2">{{ expense.name }}</div>
-          <div class="text-subtitle2">{{ expense.date }}</div>
         </q-card-section>
-
-        <q-separator />
-
-        <q-card-actions align="right">
-          <q-btn flat @click="$emit('delete-expense', expense.id)"> Delete </q-btn>
-        </q-card-actions>
       </q-card>
     </div>
   </div>
