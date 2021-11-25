8e6da050d65e8ea706d4181065900b350c12ca1f
Joe Dodgson <j.dodgson@live.co.uk>
2021-11-25 22:35:35 +0000

    Refactor EssentialLink.vue

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -3,41 +3,38 @@
     <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" />
     </q-item-section>
-
     <q-item-section>
       <q-item-label>{{ title }}</q-item-label>
       <q-item-label caption>{{ caption }}</q-item-label>
     </q-item-section>
   </q-item>
 </template>
-
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'EssentialLink',
-
+  name: "EssentialLink",
   props: {
     title: {
       type: String,
-      required: true
+      required: true,
     },
     caption: {
       type: String,
-      default: ''
+      default: "",
     },
     link: {
       type: String,
-      default: '#'
+      default: "#",
     },
     icon: {
       type: String,
-      default: ''
+      default: "",
     },
     newTab: {
       type: Boolean,
-      default: false
-    }
-  }
-})
+      default: false,
+    },
+  },
+});
 </script>
