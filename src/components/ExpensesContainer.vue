2310a76b4c089777ed45ea81b2b2112b6292c5c2
Joe Dodgson <j.dodgson@live.co.uk>
2021-12-12 18:50:15 +0000

    Remove unused dialog from ExpenseContainer

--- src/components/ExpensesContainer.vue
+++ src/components/ExpensesContainer.vue
@@ -13,22 +13,6 @@
       @update-expense="$emit('update-expense', $event)"
       @delete-expense="$emit('delete-expense', $event)"
     />
-    <q-dialog v-model="createExpenseDialog">
-      <q-card>
-        <q-card-section class="row items-center q-pb-none">
-          <div class="text-h6">New expense</div>
-          <q-space />
-          <q-btn icon="close" flat round dense v-close-popup />
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <SaveExpense
-            saveType="create"
-            @create-expense="$emit('create-expense', $event)"
-          />
-        </q-card-section>
-      </q-card>
-    </q-dialog>
   </div>
 </template>
 
